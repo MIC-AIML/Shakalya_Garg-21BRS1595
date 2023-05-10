cd683861f6e0f5aecf7cad6bf8e9a3235f4102ac
Shakalya Garg <91541420+TheShakspear@users.noreply.github.com>
2023-05-11 00:42:24 +0530

    Create index.php

---
+++ TESTING DATA/index.php
@@ -0,0 +1,16 @@
+This dataset contains 13 png files each of 28x28 pixels.
+
+Filename
+digit_image1.png
+digit_image2.png
+digit_image3.png
+digit_image4.png
+digit_image5.png
+digit_image6.png
+digit_image7.png
+digit_image8.png
+digit_image9.png
+digit_image10.png
+digit_image11.png
+digit_image12.png
+digit_image13.png
